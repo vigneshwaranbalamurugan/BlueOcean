f0560a322b68e78c3362714f8619fe1322fcf3a1
Vigneshwaran B <vigneshwaranb.22cse@kongu.edu>
2025-01-25 00:05:05 +0530

    features

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,10 +3,16 @@ import homeCard4 from "@images/homecard4.png";
 import homeCard1 from "@images/homecard1.png";
 import homeCard2 from "@images/homecard2.png";
 import homeCard3 from "@images/homecard3.png";
+import feature1 from "@images/features1.svg";
+import feature2 from "@images/features2.svg"
+import feature3 from "@images/features3.svg"
+
 import homestyles from "@styles/home.module.css";
+import featurestyles from "@styles/features.module.css";
 
 export default function Home() {
   return (
+  <>
   <div className={homestyles.home}>
 
     <div className={homestyles.rectangle}><Image src={homeCard2} width={250} height={250}   alt="Youtube Homecard"/></div>
@@ -15,11 +21,11 @@ export default function Home() {
       <h1>
           <div className={homestyles.row}>
               <p>Manage Your</p>
-              <Image src={homeCard4} width={100} height={100} alt="Youtube Homecard" />
+              <Image src={homeCard4} width={150} height={150} alt="Youtube Homecard" />
               <h2>Youtube</h2>
           </div>
           <div className={`${homestyles.row} ${homestyles.highlight}`}>
-              <Image src={homeCard1} width={200} height={200} alt="Youtube Homecard" />
+              <Image src={homeCard1} width={300} height={300} alt="Youtube Homecard" />
               <p>and Permissions</p>
           </div>
       </h1>
@@ -28,5 +34,29 @@ export default function Home() {
       </h5>
       <button><p className={homestyles.circle}></p>Try Out</button>
     </div>
+
+    <div className={featurestyles.feature}>
+            <h5><p className={featurestyles.circle}></p>Features</h5>
+            <p className={featurestyles.description}>The features Both familiar and new.</p>
+
+            <div className={featurestyles.features}>  
+                <div>
+                  <h1><p className={featurestyles.circle}></p>Youtube Integration</h1>
+                  <p>Connect your channel and manage uploads directly.</p>
+                  <Image src={feature1} className={featurestyles.image} alt="Feature Image"/>
+                </div>
+                <div>
+                  <h1><p className={featurestyles.circle}></p>Cloud Storage</h1>
+                  <p>Organize and secure your videos with scalable storage.</p>
+                  <Image src={feature2} className={featurestyles.image} alt="Feature Image"/>
+                </div>
+                <div>
+                  <h1><p className={featurestyles.circle}></p>Permission Control</h1>
+                  <p>Set custom access levels for collaborators and viewers.</p>
+                  <Image src={feature3} className={featurestyles.image} alt="Feature Image"/>
+                </div>
+            </div>
+    </div>
+    </>
   );
 }
